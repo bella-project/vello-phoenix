59d435ad0a6a2d48c4d4169a72c651b484c18296
TheNachoBIT <nacholopezcorvalan2@gmail.com>
2025-02-09 17:02:28 -0300

    Applied cargo fmt

--- catalina/src/lib.rs
+++ catalina/src/lib.rs
@@ -171,7 +171,10 @@ use wgpu::{Device, Queue, SurfaceTexture, TextureFormat, TextureView};
 #[cfg(all(feature = "wgpu", feature = "wgpu-profiler"))]
 use wgpu_profiler::{GpuProfiler, GpuProfilerSettings};
 
-#[allow(unused_imports, reason = "There's nothing more permanent than a temporary solution.")]
+#[allow(
+    unused_imports,
+    reason = "There's nothing more permanent than a temporary solution."
+)]
 use hashbrown::HashMap;
 
 /// Represents the anti-aliasing method to use during a render pass.
