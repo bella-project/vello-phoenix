cf0a9146c796c3b5acda309c8cf29bd80e3644a7
TheNachoBIT <nacholopezcorvalan2@gmail.com>
2025-02-09 12:19:39 -0300

    Fix clippy v2

--- catalina/src/lib.rs
+++ catalina/src/lib.rs
@@ -171,7 +171,7 @@ use wgpu::{Device, Queue, SurfaceTexture, TextureFormat, TextureView};
 #[cfg(all(feature = "wgpu", feature = "wgpu-profiler"))]
 use wgpu_profiler::{GpuProfiler, GpuProfilerSettings};
 
-#[allow(unused_imports)]
+#[cfg(feature = "wgpu")]
 use hashbrown::HashMap;
 
 /// Represents the anti-aliasing method to use during a render pass.
